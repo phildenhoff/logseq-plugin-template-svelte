9b271def357e636b76a42fb3962b72b02bf9d34a
Phil Denhoff <phil@denhoff.ca>
2022-10-11 20:59:32 -0700

    refactor: rename vite.svg to logo.svg & use for package

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,10 @@
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/logo.svg" class="logo" alt="Vite Logo" />
     </a>
-    <a href="https://svelte.dev" target="_blank"> 
+    <a href="https://svelte.dev" target="_blank">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
